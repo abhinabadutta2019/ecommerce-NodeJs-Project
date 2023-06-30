6c33d5cd2a0fbc2b97112970e51e28aaa6533706
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-06-30 22:08:19 +0530

    login route, upto bcrypt.compare()

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+//
+
 const User = require("../models/User");
+
+//
 
 //
 const { hashPass } = require("../helper/utlis");
@@ -19,7 +24,51 @@ router.post("/register", async (req, res) => {
     });
     //
     const user = await newUser.save();
-    res.json(user);
+    res.json({ user: user, message: "registration success" });
+  } catch (err) {
+    console.log(err);
+    res.json(err);
+  }
+});
+
+//
+router.post("/login", async (req, res) => {
+  try {
+    // console.log(req.body.username, "req.body.username");
+    // console.log(req.body.password, "req.body.password");
+    //
+    const inputUsername = req.body.username;
+    const inputPassword = req.body.password;
+    const stringPassword = inputPassword.toString();
+
+    // console.log(stringPassword, "stringPassword");
+
+    //
+    const user = await User.findOne({
+      username: inputUsername,
+    });
+
+    // console.log(user, "user");
+    const hashedPassFromDB = user.password;
+
+    // chaek if username found
+    if (!user) {
+      return res.json({ message: "Username not in database" });
+    }
+    //
+    const match = await bcrypt.compare(stringPassword, hashedPassFromDB);
+
+    //check if password matched
+    if (!match) {
+      return res.json({ message: "password not matched" });
+    }
+
+    console.log(match, "match");
+
+    //
+    res.json({ user: user, message: "login success" });
+    // res.json()
+    //
   } catch (err) {
     console.log(err);
     res.json(err);
