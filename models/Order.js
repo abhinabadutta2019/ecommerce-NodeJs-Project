b711416af94b9abf8b1b561a13db3cae5b8f009e
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-07-09 20:28:12 +0530

    created  order schema/model

---
+++ models/Order.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const User = require("../models/User");
+const Product = require("../models/Product");
+const Cart = require("../models/Cart");
+//
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    products: [
+      {
+        _id: false,
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+      },
+    ],
+    amount: { type: Number, required: true },
+    address: { type: Object, required: true },
+    status: { type: String, default: "pending" },
+  },
+
+  { timestamps: true }
+);
+
+//
+const Order = mongoose.model("Order", orderSchema);
+module.exports = Order;
