3bf26d826d86aaea39f7a52c293116ca9cac7c12
abhinabadutta2019 <abhinabadutta2019@gmail.com>
2023-07-20 22:10:57 +0530

    addreess schema created

---
+++ models/Address.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const User = require("../models/User");
+//
+
+const addressSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  addressDetail: [
+    {
+      location: {
+        type: String,
+        required: true,
+      },
+    },
+  ],
+});
+
+const Address = mongoose.model("Address", addressSchema);
+module.exports = Address;
